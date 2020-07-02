d61e71efff83f5200cf9bfde1ecd09dfc1e8a90a
kate2513 <katy.s.golovina@gmail.com>
2020-07-02 14:18:07 +0300

    fixed code v3

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const github = require('@actions/github');
 
 async function run() {
 	try {	  
-	  const messageToSent = core.getInput('message');
+	  const message = core.getInput('message');
 	  const github_token = core.getInput('GITHUB_TOKEN');
+	  
 	  const context = github.context;
 	  
 	  if (context.payload.pull_request == null){
@@ -12,9 +13,10 @@ async function run() {
 		  return;
 	  }
 	  
-	  const pull_request_number = context.payload.pull_request_number;
+	  const pull_request_number = context.payload.pull_request.number;
 	  
-	  const octokit = github.getOctokit(github_token);
+	  const octokit = new github.GitHub(github_token);
+	  
 	  const comment = octokit.issues.createComment({
 		  ...context.repo,
 		  issue_number: pull_request_number,
