c3fdfefaa556f2f880bbb0c7e56899a5d7d0d13e
kate2513 <katy.s.golovina@gmail.com>
2020-07-02 14:13:11 +0300

    fixed code v2

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  
-  const messageToSent = core.getInput('message');
-  const github_token = core.getInput('GITHUB_TOKEN');
-  const context = github.context;
-  
-  if (context.payload.pull_request == null){
-	  core.setFailed('No PR found!');
-	  return;
-  }
-  
-  const pull_request_number = context.payload.pull_request_number;
-  
-  const octokit = github.getOctokit(github_token);
-  const comment = octokit.issues.createComment({
-	  ...context.repo,
-	  issue_number: pull_request_number,
-	  body: message
-  });  
+async function run() {
+	try {	  
+	  const messageToSent = core.getInput('message');
+	  const github_token = core.getInput('GITHUB_TOKEN');
+	  const context = github.context;
+	  
+	  if (context.payload.pull_request == null){
+		  core.setFailed('No PR found!');
+		  return;
+	  }
+	  
+	  const pull_request_number = context.payload.pull_request_number;
+	  
+	  const octokit = github.getOctokit(github_token);
+	  const comment = octokit.issues.createComment({
+		  ...context.repo,
+		  issue_number: pull_request_number,
+		  body: message
+	  });  
 
-} catch (error) {
-  core.setFailed(error.message);
+	} catch (error) {
+	  core.setFailed(error.message);
+	}
 }
+
+run();
